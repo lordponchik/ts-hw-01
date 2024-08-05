98fd708739d3b74b6d8b65bcd6ace336556a29cc
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-05 12:01:00 +0200

    add task-01

---
+++ src/task-01.ts
@@ -0,0 +1,11 @@
+console.log("task-01");
+
+const name: string = "Генератор защитного поля";
+let price: number = 1000;
+
+console.log(`Выбран «${name}», цена за штуку ${price} кредитов.`);
+
+price = 2000;
+
+console.log(`Выбран «${name}», цена за штуку ${price} кредитов.`);
+console.log("\n");
