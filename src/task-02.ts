5a626ce9fd6d0664b6b8d78673b1cd347001df04
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-05 12:08:41 +0200

    add task-02

---
+++ src/task-02.ts
@@ -0,0 +1,35 @@
+console.log('task-02');
+
+const total: number = 100;
+let ordered: number = 50;
+
+console.log(`total = ${total}; ordered = ${ordered}`);
+console.log(
+  ordered > total ? 'На складе недостаточно твоаров!' : 'Заказ оформлен, с вами свяжется менеджер'
+);
+console.log('\n');
+
+ordered = 20;
+
+console.log(`total = ${total}; ordered = ${ordered}`);
+console.log(
+  ordered > total ? 'На складе недостаточно твоаров!' : 'Заказ оформлен, с вами свяжется менеджер'
+);
+console.log('\n');
+
+ordered = 80;
+
+console.log(`total = ${total}; ordered = ${ordered}`);
+console.log(
+  ordered > total ? 'На складе недостаточно твоаров!' : 'Заказ оформлен, с вами свяжется менеджер'
+);
+console.log('\n');
+
+ordered = 130;
+
+console.log(`total = ${total}; ordered = ${ordered}`);
+console.log(
+  ordered > total ? 'На складе недостаточно твоаров!' : 'Заказ оформлен, с вами свяжется менеджер'
+);
+
+console.log('\n');
